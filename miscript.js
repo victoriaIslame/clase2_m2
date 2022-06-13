fc0069f50739362c9339fca35f68418411ac0995
maria victoria <vipurplerose@gmail.com>
2022-06-13 17:13:35 -0400

    clase 2 modulo 2

---
+++ miscript.js
@@ -0,0 +1,24 @@
+let edad = prompt ("Indique su edad")
+const mensaje = ("tu edad es"+ edad + "años");
+console.log(mensaje);
+alert(mensaje);
+edad = parseInt (edad)
+let genero = prompt ("indica si eres mujer o hombre");
+const mensaje2 = "tu género es" + genero;
+console.log(mensaje2);
+alert(mensaje2);
+
+if (genero == mujer)
+    if (edad>=60){
+    console.log("Usted puede jubilarse");
+alert("Usted puede jubilarse")
+    } 
+    else{
+        if (edad<60) {
+            console.log("usted no puede jubilarse")
+            alert("usted no puede jubilarse")
+        }
+    }
+
+
+
